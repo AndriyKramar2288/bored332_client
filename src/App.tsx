f0c5642b6a7d8e9bc54a08ce2a472450cb64c84c
Andriyko <andriykramar465@gmail.com>
2025-10-05 15:27:41 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+
+function App() {
+  return (
+    <>
+      <div className=''>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
